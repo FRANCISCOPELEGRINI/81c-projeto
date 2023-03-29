f6b5f58bc02094cfae85a10d84055877bb02afb4
FRANCISCOPELEGRINI <114108940+FRANCISCOPELEGRINI@users.noreply.github.com>
2023-03-29 16:16:01 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-canvas= document.getElementById("mycanvas");
+canvas= document.getElementById("myCanvas");
 ctx= canvas.getContext("2d");
 
 ctx.beginPath();
@@ -8,7 +8,31 @@ ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
 ctx.beginPath();
+ctx.strokeStyle= "yellow";
+ctx.lineWidth= 5;
+ctx.arc(210, 200, 40, 0, 2* Math.PI)
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle= "black";
+ctx.lineWidth= 5;
+ctx.arc(390, 200, 40, 0, 2* Math.PI)
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle= "green";
+ctx.lineWidth= 5;
+ctx.arc(250, 250, 40, 0, 2* Math.PI)
+ctx.stroke()
+
+ctx.beginPath();
+ctx.strokeStyle= "blue";
+ctx.lineWidth= 5;
+ctx.arc(350, 250, 40, 0, 2* Math.PI)
+ctx.stroke()
+
+ctx.beginPath();
 ctx.strokeStyle= "red";
 ctx.lineWidth= 5;
 ctx.arc(300, 200, 40, 0, 2* Math.PI)
-ctx.stroke
+ctx.stroke()
